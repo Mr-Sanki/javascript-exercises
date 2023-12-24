1061e5570237f4cf17b9ea0ecc1ead82eca7d82a
wajih <wajih_email@gmail.com>
2023-12-24 17:22:37 +0100

    Exercice DONE

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+  if (num<0) {return 'ERROR'}
+  let str = '';
+  for (let i=0; i<num; i++){
+    str += text;
+  }  
+  return str;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
