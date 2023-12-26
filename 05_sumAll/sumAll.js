23065cbc0a0a046e57ce017a235275564b6174a2
wajih <wajih_email@gmail.com>
2023-12-26 19:13:13 +0100

    exercices done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(minNum, maxNum) {
+    if (minNum<0 || maxNum<0 || typeof minNum !== "number" || typeof maxNum!== "number") {
+        return 'ERROR'
+    } else {
+        let sum =0;
+        let newMin = Math.min(minNum, maxNum);
+        let newMax = Math.max(minNum, maxNum);
+        for (let i = newMin; i <= newMax; i++) {
+            sum+=i;
+            
+        }
+        return sum;
+    }
+    
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
