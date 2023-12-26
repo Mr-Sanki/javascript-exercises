94dbdf7a512ffc2ed61e9e65c1e853709173cdc9
wajih <wajih_email@gmail.com>
2023-12-26 18:46:30 +0100

    Exercice 04 Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-    
-};
+const removeFromArray = function(arr, ...arrgs) {
+    for (let i = 0; i < arrgs.length; i++) {
+        const index = arr.indexOf(arrgs[i]);
+        if (index !== -1) {
+            arr.splice(index, 1);
+          }
+    }
+    return arr;
+};  
 
 // Do not edit below this line
 module.exports = removeFromArray;
